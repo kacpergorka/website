c895a1ce475a4b0fff73efc80c759c9f895a6458
Kacper Górka <166524993+kacpergorka@users.noreply.github.com>
2025-05-13 21:48:42 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,46 +1,30 @@
-let isTooltipVisible = false;
-
 tailwind.config = {
-  darkMode: "class"
+	darkMode: "class"
 };
 
 function toggleTheme() {
-  document.documentElement.classList.toggle("dark");
-  updateThemeColor();
+	const isDark = document.documentElement.classList.toggle("dark");
+	localStorage.setItem("theme", isDark ? "dark" : "light");
+	updateThemeColor();
 }
 
 function updateThemeColor() {
-  const themeColorMeta = document.getElementById("theme-color-meta");
-  const isDark = document.documentElement.classList.contains("dark");
-  themeColorMeta.setAttribute("content", isDark ? "#111827" : "#ffffff");
+	const themeColorMeta = document.getElementById("theme-color-meta");
+  	const isDark = document.documentElement.classList.contains("dark");
+  	if (themeColorMeta) {
+		themeColorMeta.setAttribute("content", isDark ? "#111827" : "#ffffff");
+	}
 }
 
-function showTooltip(tooltip) {
-  tooltip.classList.add("opacity-100");
-  tooltip.classList.remove("opacity-0");
-  isTooltipVisible = true;
-}
+document.addEventListener("DOMContentLoaded", () => {
+	const savedTheme = localStorage.getItem("theme");
+	const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-function hideTooltip(tooltip) {
-  tooltip.classList.add("opacity-0");
-  tooltip.classList.remove("opacity-100");
-  isTooltipVisible = false;
-}
+	if (savedTheme === "dark" || (!savedTheme && prefersDark)) {
+		document.documentElement.classList.add("dark");
+  	} else {
+		document.documentElement.classList.remove("dark");
+	}
 
-function toggleDiscordTooltip() {
-  const tooltip = document.getElementById("discordTooltip");
-  if (window.innerWidth > 768) return;
-
-  isTooltipVisible ? hideTooltip(tooltip) : showTooltip(tooltip);
-}
-
-document.addEventListener("click", function (event) {
-  const icon = document.getElementById("discordIcon");
-  const tooltip = document.getElementById("discordTooltip");
-
-  if (isTooltipVisible && !icon.contains(event.target)) {
-    hideTooltip(tooltip);
-  }
-});
-
-document.addEventListener("DOMContentLoaded", updateThemeColor);
+	updateThemeColor();
+})
