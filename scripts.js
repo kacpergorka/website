78e0f6fe1f1c716f73682f817dbce4588a230efa
kacpergorka <166524993+kacpergorka@users.noreply.github.com>
2026-03-02 16:54:15 +0100

    optimization, cleaning and adding new redirects

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const link = document.getElementById("email");
     if (!link) return;
 
-    const encoded = "a29udGFrdEBrYWNwZXJnb3JrYS5jb20=";
-    link.href = `mailto:${atob(encoded)}`;
+    const zakodowany = "a29udGFrdEBrYWNwZXJnb3JrYS5jb20=";
+    link.href = `mailto:${atob(zakodowany)}`;
 });
